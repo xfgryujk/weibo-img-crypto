b5359603309d4521e79c52a2edb2f719f61e03c4
John Smith <xfgryujk@126.com>
2018-07-14 13:17:40 +0800

    添加解密

---
+++ README.md
@@ -0,0 +1,9 @@
+# weibo-img-crypto
+自动加密解密微博上传的图片
+
+## 如何使用
+在浏览器地址栏输入以下代码，上传图片时就会自动加密，在图片上点击鼠标右键就会自动解密。注意前面的 "`javascript:`" 必须手动输入，不能复制粘贴，否则会被浏览器自动去掉
+
+```javascript
+javascript:fetch('https://raw.githubusercontent.com/xfgryujk/weibo-img-crypto/master/weibo-img-crypto.js').then(res => res.text(), e => alert('载入失败：' + e)).then(res => {let script = document.createElement('script'); script.innerHTML = res; document.body.appendChild(script)})
+```
--- weibo-img-crypto.js
+++ weibo-img-crypto.js
@@ -1,6 +1,10 @@
 window.isWbImgCryptoLoaded || (function () {
   window.isWbImgCryptoLoaded = true
 
+  let canvas = document.createElement('canvas')
+  let ctx = canvas.getContext('2d')
+
+  // TODO 使用更高级的加密
   function encrypt (data) {
     const KEY = 0xAA
     for (let i = 0; i < data.length; i += 4) {
@@ -9,6 +13,7 @@ window.isWbImgCryptoLoaded || (function () {
       data[i + 2] ^= KEY
     }
   }
+  let decrypt = encrypt
 
   // Hook FileReader.readAsDataURL
   let originalReadAsDataURL = window.FileReader.prototype.readAsDataURL
@@ -19,9 +24,7 @@ window.isWbImgCryptoLoaded || (function () {
       this.onloadend = () => {
         let img = new window.Image()
         img.onload = () => {
-          let canvas = document.createElement('canvas');
           [canvas.width, canvas.height] = [img.width, img.height]
-          let ctx = canvas.getContext('2d')
           ctx.drawImage(img, 0, 0)
 
           // 加密
@@ -37,4 +40,37 @@ window.isWbImgCryptoLoaded || (function () {
     }
     originalReadAsDataURL.call(this, file)
   }
+
+  // 监听右键菜单
+  document.addEventListener('contextmenu', event => {
+    if (event.target instanceof window.Image) {
+      // event.preventDefault() // 为了右键保存图片这里先注释掉了
+      let originImg = event.target
+      if (!(originImg instanceof window.Image)) {
+        return
+      }
+
+      // 跨域
+      let img = new window.Image()
+      img.crossOrigin = 'anonymous'
+      img.onerror = () => window.alert('载入图片失败，可能是跨域问题？')
+      img.onload = () => {
+        [canvas.width, canvas.height] = [img.width, img.height]
+        ctx.drawImage(img, 0, 0)
+
+        // 解密
+        let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        decrypt(imgData.data)
+        ctx.putImageData(imgData, 0, 0)
+        originImg.src = canvas.toDataURL()
+      }
+
+      if (originImg.src.startsWith('data:')) {
+        img.src = originImg.src
+      } else {
+        // 防缓存
+        img.src = originImg.src + (originImg.src.indexOf('?') === -1 ? '?_t=' : '&_t=') + new Date().getTime()
+      }
+    }
+  })
 })()
