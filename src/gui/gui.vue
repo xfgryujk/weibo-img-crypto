6be7e78e18afb8de6e37f73b69baaad6139f5eed
John Smith <xfgryujk@126.com>
2018-07-22 13:57:05 +0800

    在新对话框中打开选择图片的大图
    修复了Edge无法保存图片的问题

--- src/gui/gui.vue
+++ src/gui/gui.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-tooltip class="item" content="发色图救救北极熊">
-      <a href="#" @click.prevent="openGui" style="position: fixed; left: 0; bottom: 0">
+      <a href="#" @click.prevent="openGui" class="gui-button">
         <img src="https://ww1.sinaimg.cn/mw690/be15a4ddjw8fbet7h4rpoj209z0c7q39.jpg" width="40px" height="49px">
       </a>
     </el-tooltip>
@@ -102,7 +102,13 @@ export default {
 </script>
 
 <style scoped>
+.gui-button {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+}
+
 .child-mb * {
-  margin-bottom: 1em
+  margin-bottom: 1em;
 }
 </style>
